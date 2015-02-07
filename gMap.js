a2163f77e57d0951ba6ccc3957c3f9d9590b47f4
Elana Stroud <estroud1@jhu.edu>
2015-02-07 01:50:09 -0500

    adding getting directions

--- gMap.js
+++ gMap.js
@@ -5,20 +5,48 @@
  * street view directions
  */
 
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
 function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+
 	var mapOptions = {
 		center: { lat: -34.397, lng: 150.644},
 		zoom: 8
 	};
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    directionsDisplay.setMap(map);
 }
 
-function calcDest() {
-    // var curr = document.getElementById('curr').value;
-    var dest = document.getElementById('dest').value; 
+// function initialize() {
+//   directionsDisplay = new google.maps.DirectionsRenderer();
+//   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+//   var mapOptions = {
+//     zoom:7,
+//     center: chicago
+//   }
+//   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+//   directionsDisplay.setMap(map);
+// }
 
-    // console.log("curr:" + curr);
-    console.log("dest:" + dest);
+
+function calcDest() {
+    var curr = document.getElementById('start').value;
+    var dest = document.getElementById('end').value; 
+
+    var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
 
 }
 
