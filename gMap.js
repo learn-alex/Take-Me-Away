a9280430155e6f36f4275bf29c25e5e90cb6e14e
Elana Stroud <estroud1@jhu.edu>
2015-02-07 04:57:39 -0500

    fix

--- gMap.js
+++ gMap.js
@@ -163,8 +163,8 @@ var bridge = new OculusBridge( {
         // giantSquid.quaternion.set(quatValues.x, quatValues.y, quatValues.z, quatValues.w);
         console.log("X: " + 180*quatValues.x);
         console.log("Y: " + 180*quatValues.y);
-        console.log("Z: " + 180*quatValues.y);
-        console.log("W: " + 180*quatValues.y);
+        console.log("Z: " + 180*quatValues.z);
+        console.log("W: " + 180*quatValues.w);
 
         if (180*quatValues.x > 0) {
             moveForward();
