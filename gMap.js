8b01e5ccd8aaac41b47a84816a7c977391809bc3
Michael Neil Miller <mmill179@ugrad3.cs.jhu.edu>
2015-02-07 00:58:45 -0500

    files

---
+++ gMap.js
@@ -0,0 +1,17 @@
+/*
+ * Google Maps API
+ * HopHacks Spring 2015
+ *
+ * street view directions
+ */
+
+function initialize() {
+	var mapOptions = {
+		center: { lat: -34.397, lng: 150.644},
+		zoom: 8
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+    
