3ae4a7c4266dc5103c45623ed33c9048c8f5a8c1
Elana Stroud <estroud1@jhu.edu>
2015-02-07 01:56:21 -0500

    functional google maps

--- gMap.js
+++ gMap.js
@@ -20,39 +20,26 @@ function initialize() {
     directionsDisplay.setMap(map);
 }
 
-// function initialize() {
-//   directionsDisplay = new google.maps.DirectionsRenderer();
-//   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-//   var mapOptions = {
-//     zoom:7,
-//     center: chicago
-//   }
-//   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-//   directionsDisplay.setMap(map);
-// }
-
-
 function calcDest() {
-    var curr = document.getElementById('start').value;
-    var dest = document.getElementById('end').value; 
+
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('finish').value; 
 
     var request = {
-    origin:start,
-    destination:end,
-    travelMode: google.maps.TravelMode.DRIVING
+        origin:start,
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
   };
 
   directionsService.route(request, function(result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(result);
+    } else {
+        window.alert("invalid loc");
     }
   });
 
 }
-
-
-
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
     
